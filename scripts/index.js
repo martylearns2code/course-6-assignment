17dac41a730e4ee340960fa3e64f92f5ea75bfca
moorthybasava <moorthybasava@hotmail.com>
2020-07-13 19:48:50 +0530

    Implemented functional logic for Allposts and CreatePost buttons --made changes to index.html,index.js,index.css and common.css --included the postslist.html for the Allposts button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-will add later
+function allPostsRedirect() {
+	 window.location.href = "C:/Users/hp/course-6-assignment/html/postslist.html";
+}      
+function createPost() {
+     var createPostmodal = document.getElementById("createPostModal");
+     createPostmodal.style.display="block";	 
+      
+}      
+function closePostModal(){
+	var createPostmodal = document.getElementById("createPostModal");
+    createPostmodal.style.display="none";  
+}		
