ad40414ae21db09ee3e17f964be0858d8803e9cc
moorthybasava <moorthybasava@hotmail.com>
2020-07-18 13:03:53 +0530

    The logical implementation of Part-C has been completed --edit function of the post.html implemented --likes function of the post.html implemented --comments function of the post.html implemented --changes implemented in post.js,post.css and post.html

--- scripts/post.js
+++ scripts/post.js
@@ -1,38 +1,70 @@
-var headerElement = document.getElementById("titleOfPost");
-var textElement = document.getElementById("contents");
-var buttonElement;
-var likesForPostCounter=0;
+var titleElement;
+var postElement;
+var editAndSaveButton;
+var likesCounter=0;
+var likesButton;
+var likesComment;
+var comments;
+var commentAdded;
 
-function onLoadOfPage(){
-   
-}
+
 function editPost(){
-	alert("clicked");
-	buttonElement=document.getElementById("editAndSave");
-	var operation=buttonElement.innerHTML;
+	editAndSaveButton=document.getElementById("editAndSave");
+	var operation=editAndSaveButton.innerHTML;
 	if(operation=="Edit"){
 	
-	textElement=document.getElementById("contents");
-	textElement.readOnly=false;
-	textElement.style.borderStyle="solid";
-	textElement.style.borderColor="pink";
-	headerElement=document.getElementById("titleOfPost");
-	headerElement.setAttribute("contentEditable","true");
-	headerElement.style.borderStyle="solid";
-	headerElement.style.borderColor="pink";
-	buttonElement.setAttribute("class","fa fa-save");
-	buttonElement.innerHTML="Save";
+	postElement=document.getElementById("contents");
+	postElement.readOnly=false;
+	postElement.style.borderStyle="solid";
+	postElement.style.borderColor="pink";
+	titleElement=document.getElementById("titleOfPost");
+	titleElement.setAttribute("contentEditable","true");
+	titleElement.style.borderStyle="solid";
+	titleElement.style.borderColor="pink";
+	editAndSaveButton.setAttribute("class","fa fa-save");
+	editAndSaveButton.innerHTML="Save";
 	}else{
-	buttonElement.setAttribute("class","fa fa-edit");
-	buttonElement.innerHTML="Edit";
-    var updatedTitle= headerElement.innerHTML;
-	headerElement.innerHTML=updatedTitle;
-    textElement.innerHTML=textElement.value;
-    headerElement.setAttribute("contentEditable","false");
-    textElement.readOnly=true;
-    headerElement.style.borderStyle="none";	
-	headerElement.style.borderColor="none";
-	textElement.style.borderColor="black";
+	editAndSaveButton.setAttribute("class","fa fa-edit");
+	editAndSaveButton.innerHTML="Edit";
+    var updatedTitle= titleElement.innerHTML;
+	titleElement.innerHTML=updatedTitle;
+    postElement.innerHTML=postElement.value;
+    titleElement.setAttribute("contentEditable","false");
+    postElement.readOnly=true;
+    titleElement.style.borderStyle="none";	
+	titleElement.style.borderColor="none";
+	postElement.style.borderColor="black";
 		
 	}
+}
+function likesForPost(){
+	if(likesCounter==0){
+		likesCounter=likesCounter+1;
+		likesButton=document.getElementById("likesButton");
+		likesButton.innerHTML="Liked";
+		likesComment=document.getElementById("likesComment");
+		likesComment.innerHTML="1 person likes this!";
+		
+	}else{
+		likesCounter = likesCounter + 1;
+		likesComment.innerHTML= likesCounter + " people like this!";
+		
+	}
+		
+}
+function commentsOnPost(){
+	comments = document.getElementById("comments");
+	var commentAdded=document.createElement('p');
+	commentAdded.style.backgroundColor="white";
+	commentAdded.innerHTML=comments.value;
+	commentAdded.paddingBottom="15px";
+	commentAdded.paddingTop="15px";
+	var addComments=document.getElementById("addComments");
+	addComments.style.borderStyle="groove";
+	addComments.style.borderColor="#ebebe0";
+	addComments.appendChild(commentAdded);
+    comments.defaultValue="Leave a comment...";
+	comments.value=comments.defaultValue;
+	
+	
 }
